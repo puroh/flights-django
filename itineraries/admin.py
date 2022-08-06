3e878f58f9dcb190d247970640c72d9d27ba1baa
purohr <diego683@gmail.com>
2022-08-06 15:37:29 -0500

    added inlines to admin

--- itineraries/admin.py
+++ itineraries/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
 from itineraries.models import Itinerary
+from legs.models import Leg
+
+
+class ItinerariesInline(admin.TabularInline):
+    model = Leg.itineraries.through
+    extra = 3
 
 
 @admin.register(Itinerary)
 class LegAdmin(admin.ModelAdmin):
     # fields = ()
+    inlines = [
+        ItinerariesInline,
+    ]
     list_display = (
         "agent",
         "agent_rating",
