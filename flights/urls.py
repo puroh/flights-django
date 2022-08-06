d5020eb06616a84d39c7808025cd02c4d9164ccc
purohr <diego683@gmail.com>
2022-08-06 03:24:24 -0500

    base urls

--- flights/urls.py
+++ flights/urls.py
@@ -5,20 +5,29 @@ from django.contrib import admin
 from django.views.generic.base import RedirectView
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken import views
+
+
 from .users.views import UserViewSet, UserCreateViewSet
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 router = DefaultRouter()
-router.register(r'users', UserViewSet)
-router.register(r'users', UserCreateViewSet)
+router.register(r"users", UserViewSet)
+router.register(r"users", UserCreateViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/v1/', include(router.urls)),
-    path('api-token-auth/', views.obtain_auth_token),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-
+    path("admin/", admin.site.urls),
+    path("api/v1/", include(router.urls)),
+    path("api-token-auth/", views.obtain_auth_token),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     # the 'api-root' from django rest-frameworks default router
     # http://www.django-rest-framework.org/api-guide/routers/#defaultrouter
-    re_path(r'^$', RedirectView.as_view(url=reverse_lazy('api-root'), permanent=False)),
-
+    re_path(r"^$", RedirectView.as_view(url=reverse_lazy("api-root"), permanent=False)),
+    # my urls
+    path("api/v1/", include("itineraries.urls")),
+    path("api/v1/", include("legs.urls")),
+    # OpenAPI 3.0 Schema
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    # API Docs:
+    path("api/swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger"),
+    path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
